b53d4b0ddf67b1de30ed1ff4e01fa814f859bede
Baz <bazyl.rei@gmail.com>
2024-11-02 22:20:33 +0100

    adds results

--- premier/src/app/components/home/home.component.ts
+++ premier/src/app/components/home/home.component.ts
@@ -1,16 +1,44 @@
 import { Component } from "@angular/core";
+import { KeywordService } from "../../services/keyword-service.service";
+import { ArticleService } from "../../services/article-service.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
   searchQuery: string = '';
+  formattedKeywordSets: string[] = [];
+  articleData: any[] = []; // To store the article and validation data
+
+  constructor(private keywordService: KeywordService, private articleService: ArticleService) { }
 
   onSearch() {
     console.log('Searching for:', this.searchQuery);
-    // Handle search logic here
+    this.keywordService.search(this.searchQuery).subscribe(
+      response => {
+        console.log(response);
+        this.formattedKeywordSets = response.keywordSets.map(
+          (set: { keywords: string[] }) => set.keywords.map(keyword => `"${keyword}"`).join(' ')
+        );
+      },
+      error => {
+        console.error('Search error:', error);
+      }
+    );
+  }
+
+  onKeywordClick(keyword: string) {
+    console.log('Keyword clicked:', keyword);
+    this.articleService.fetch(keyword).subscribe(
+      response => {
+        console.log(response);
+        this.articleData = response; // Store response directly for display
+      },
+      error => {
+        console.error('Search error:', error);
+      }
+    );
   }
 }
-
--- premier/src/app/services/article-service.service.ts
+++ premier/src/app/services/article-service.service.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class KeywordService {
+export class ArticleService {
   private apiUrl = 'http://0.0.0.0:8000'; // Update to your service URL
 
   constructor(private http: HttpClient) { }
 
-  search(keyword: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/generate_keywords`, { params: { keyword } });
+  fetch(keyword: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/search_articles`, { params: { keyword } });
   }
 }
